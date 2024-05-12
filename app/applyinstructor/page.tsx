55b8e801d2d8070ea11ccc9a6539bca4e02a9f0b
Hossain, Musharuf <21-45112-2@student.aiub.edu>
2024-05-13 05:30:06 +0600

    instructor

---
+++ app/applyinstructor/page.tsx
@@ -0,0 +1,29 @@
+
+
+import ApplyInstructor from "../components/ApplyInstructor/ApplyInstructor";
+
+import HeroArea from "../components/HeroArea";
+
+export default function applyinstructor() {
+  return (
+    <>
+      <HeroArea
+        title="Teach with Us: Apply to be an Instructor"
+        description="Apply to become an instructor on our platform! We're looking for passionate individuals like you to teach and inspire learners worldwide. Show us what you know, and help shape the future of online education. Apply now and start making a difference!."
+      />
+      <div className="container mx-auto">
+        <div className="flex flex-row">
+          <div className="basis-3/12 pr-10">
+            <div className="py-16">
+              <ApplyInstructor />
+             
+            </div>
+          </div>
+          <div className="basis-9/12">
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- app/components/NavBar/NavLink.tsx
+++ app/components/NavBar/NavLink.tsx
@@ -33,6 +33,16 @@ export default function NavLink() {
         Support Tickets
       </Link>
       <Link
+        href="/applyinstructor"
+        className={`text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium ${
+          pathname === "/applyinstructor" ? "bg-gray-900" : ""
+        }`}
+      >
+       Apply Instructor
+      </Link>
+
+  
+      <Link
         href="/dashboard"
         className={`text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium ${
           pathname === "/dashboard" ? "bg-gray-900" : ""
