e4d9481f31e8d496aab1c4d87cf73bd867e5d92f
rayhankabir-me <devrayhankabir@gmail.com>
2024-05-14 03:02:44 +0600

    login successfull

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -51,7 +51,7 @@ export default function Register() {
             type="username"
             id="username"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            placeholder="jhone"
+            placeholder="jhon"
           />
           {errors.username?.message && (
             <p className="mt-2 text-sm text-red-600 dark:text-red-500">
