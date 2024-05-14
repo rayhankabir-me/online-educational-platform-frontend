8e212fec827428e8907328a63a42dbdd1cc3c2c4
rayhankabir-me <devrayhankabir@gmail.com>
2024-05-15 05:24:41 +0600

    showing category in dashboard by mrittika

---
+++ app/dashboard/allcategories/page.tsx
@@ -0,0 +1,197 @@
+"use client";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+import Cookies from "js-cookie";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import CourseImage from "../../../public/course_image.webp";
+export default function AllCategories() {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [accessToken, setAccessToken] = useState(null);
+  const router = useRouter();
+
+  //getting the access token for user
+  useEffect(() => {
+    const access_token = Cookies.get("access_token");
+    setAccessToken(access_token);
+  }, []);
+
+  //getting the categories data
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        const response = await axios.get(
+          process.env.NEXT_PUBLIC_BACKEND_API + "/categories/"
+        );
+        setCategories(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    }
+
+    fetchCategories();
+  }, []);
+
+  //delete course
+  const handleDeleteCourse = async (id) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this course?"
+    );
+    if (!confirmDelete) return;
+
+    try {
+      await axios.delete(
+        process.env.NEXT_PUBLIC_BACKEND_API + `/courses/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      // Refresh courses after deletion
+      const response = await axios.get(
+        process.env.NEXT_PUBLIC_BACKEND_API + "/courses/"
+      );
+      setCourses(response.data);
+    } catch (error) {
+      alert("Error deleting course:", error);
+      // Handle error here
+    }
+  };
+
+  if (loading) {
+    return (
+      <>
+        <button
+          disabled
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+        >
+          <svg
+            aria-hidden="true"
+            role="status"
+            className="inline w-4 h-4 me-3 text-white animate-spin"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="#E5E7EB"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentColor"
+            />
+          </svg>
+          Loading...
+        </button>
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <div
+        className="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+        role="alert"
+      >
+        <svg
+          className="flex-shrink-0 inline w-4 h-4 me-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+        <span className="sr-only">Info</span>
+        <div>
+          <span className="font-medium">Error!</span> {error.message}.
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-4 sm:ml-64">
+      <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+        <div className="flex items-center justify-center mb-4 rounded bg-gray-50 dark:bg-gray-800">
+          <div className="w-full relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="px-6 py-3"></th>
+                  <th scope="col" className="px-6 py-3">
+                    Image Url
+                  </th>
+
+                  <th scope="col" className="px-6 py-3">
+                    Name
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Description
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Created By
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {categories.map((category) => (
+                  <tr
+                    key={category.id}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                  >
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      <Image
+                        src={CourseImage}
+                        width={100}
+                        height={80}
+                        alt="Courese Image"
+                      />
+                    </th>
+
+                    <td className="px-6 py-4">{category.category_name}</td>
+                    <td className="px-6 py-4"> {category.description}</td>
+
+                    <td className="px-6 py-4">
+                      {category.created_by.username}
+                    </td>
+                    <td className="flex items-center px-6 py-4">
+                      <Link
+                        href={`/dashboard/update-course/${category.id}`}
+                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                      >
+                        Edit
+                      </Link>
+                      <button
+                        onClick={() => handleDeleteCourse(category.id)}
+                        className="font-medium text-red-600 dark:text-red-500 hover:underline ms-3"
+                      >
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
