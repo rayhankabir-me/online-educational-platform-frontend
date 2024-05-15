a670d9ebda81a02ec0724fe55740d0e192d599c6
WTMrittika <21-45108-2@student.aiub.edu>
2024-05-15 11:46:52 +0600

    update cat M

---
+++ app/components/CategoryPage/UpdateCategory.tsx
@@ -0,0 +1,173 @@
+"use client";
+
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+export default function UpdateCourse({ id }) {
+  const [successMessage, setSuccessMessage] = useState("");
+  const [accessToken, setAccessToken] = useState(null);
+  const [currentId, setCurrentId] = useState(id);
+
+  //setting courses data in state
+  const [categoryData, setCateoryData] = useState("");
+
+  //getting the access token for user
+  useEffect(() => {
+    const access_token = Cookies.get("access_token");
+    setAccessToken(access_token);
+  }, []);
+
+  //getting the single category data
+  useEffect(() => {
+    async function fetCourseData() {
+      try {
+        const response = await axios.get(
+          process.env.NEXT_PUBLIC_BACKEND_API + "/categories/" + currentId
+        );
+        setCateoryData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetCourseData();
+  }, [currentId]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm();
+
+  const submitForm = async (formData) => {
+    formData.updated_at = "2024-03-23T22:03:35.160Z";
+    try {
+      const response = await axios.patch(
+        process.env.NEXT_PUBLIC_BACKEND_API + "/categories/" + currentId,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        setSuccessMessage("Category updated successfully!");
+      }
+    } catch (error) {
+      const errorMessage = error.response?.data?.message || "An error occurred";
+      setError("root.random", {
+        message: errorMessage,
+        type: "random",
+      });
+    }
+  };
+
+  return (
+    <div className="p-4 sm:ml-64">
+      <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+        <div className="flex items-center justify-center mb-4 rounded bg-gray-50 dark:bg-gray-800">
+          <div className="px-4 py-8 w-full">
+            <form onSubmit={handleSubmit(submitForm)}>
+              <div className="mb-5">
+                <label
+                  htmlFor="category_name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Category Name
+                </label>
+                <input
+                  {...register("category_name", {
+                    required: "Your must fill course title...",
+                  })}
+                  type="category_name"
+                  id="category_name"
+                  name="category_name"
+                  defaultValue={categoryData.category_name}
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                />
+                {errors.category_name?.message && (
+                  <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                    <span className="font-medium">Oh, sorry!</span>{" "}
+                    {errors.category_name?.message}
+                  </p>
+                )}
+              </div>
+              <div className="mb-5">
+                <label
+                  htmlFor="description"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Description
+                </label>
+                <textarea
+                  {...register("description", {
+                    required: "Your must fill course description..",
+                  })}
+                  id="description"
+                  name="description"
+                  defaultValue={categoryData.description}
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                />
+                {errors.description?.message && (
+                  <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                    <span className="font-medium">Oh, sorry!</span>{" "}
+                    {errors.description?.message}
+                  </p>
+                )}
+              </div>
+              <div className="mb-5">
+                <label
+                  htmlFor="image_url"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Image Url
+                </label>
+                <input
+                  {...register("image", {
+                    required: "You must provide an image...",
+                  })}
+                  type="text"
+                  id="image"
+                  name="image"
+                  defaultValue={categoryData.image}
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                />
+                {errors.image?.message && (
+                  <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                    <span className="font-medium">Oh, sorry!</span>{" "}
+                    {errors.image?.message}
+                  </p>
+                )}
+              </div>
+              <button
+                type="submit"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Update Category
+              </button>
+
+              {successMessage && (
+                <div className="mt-8 flex items-center">
+                  <p className="text-sm text-green-600 dark:text-green-500">
+                    <span className="font-medium">Great!</span> {successMessage}
+                  </p>
+                </div>
+              )}
+
+              {errors?.root?.random?.message && (
+                <p className="mt-8 text-sm text-red-600 dark:text-red-500">
+                  <span className="font-medium">Oh, sorry!</span>{" "}
+                  {errors?.root?.random?.message}!
+                </p>
+              )}
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
