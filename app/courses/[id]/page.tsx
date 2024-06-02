c72bddaca1ed506cbdd1784a45396ccb9a7d6a4d
rayhankabir-me <devrayhankabir@gmail.com>
2024-06-03 00:29:09 +0600

    design changed a bit

--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -134,10 +134,15 @@ export default function CourseDetails({ params }) {
               <h3 className="mt-8">
                 Price: <strong>{courseData.price}</strong>
               </h3>
-              <h4>
+              <h4 className="mb-8">
                 Category: <strong>{courseData.category.category_name}</strong>
               </h4>
-              <Link href={`/Reviewcourse/`}>Review Course</Link>
+              <Link
+                className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                href={`/Reviewcourse/`}
+              >
+                Review Course
+              </Link>
             </div>
           </article>
         </div>
